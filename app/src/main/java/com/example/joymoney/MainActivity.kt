f0c9e5ab7c72d94a30bc275aa74640613e5debb2
Saurabh Kumar <saurabhk@beelabs.technology>
2023-09-13 15:58:47 +0530

    first commit

---
+++ app/src/main/java/com/example/joymoney/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.joymoney
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val navController = findNavController(R.id.activity_main_nav_host_fragment1)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_botton)
+        bottomNavigationView.setupWithNavController(navController)
+
+    }
+}
